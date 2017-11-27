92534a3500c506f2e6fdb6ad7a91ac805a1d0d88
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-11-27 15:27:53 -0500

    problem 4

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    public HashMap<Character,Integer> getMapOfCharacterAndAppearance(String str){
+        HashMap<Character,Integer> appearanceMap = new HashMap<Character, Integer>();
+        for(int i=0;i<str.length();i++){
+            Character currentCharacter = str.charAt(i);
+            if(appearanceMap.containsKey(currentCharacter)){
+                appearanceMap.put(currentCharacter,appearanceMap.get(currentCharacter)+1);
+            }else{
+                appearanceMap.put(currentCharacter,1);
+            }
+        }
+        return appearanceMap;
+    }
+
+    public String canStringBePalindrome(String str){
+        int numberOfCharactersWithOddNumberAppearances = 0;
+        for(Integer currentValue : getMapOfCharacterAndAppearance(str).values()){
+            if(currentValue % 2 != 0){
+                numberOfCharactersWithOddNumberAppearances ++;
+            }
+        }
+        //if more then 1 character has an odd number of appearance the string can not be a palindrome
+        return numberOfCharactersWithOddNumberAppearances > 1 ? "NO" : "YES";
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,45 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+    Problem4 problem4 = new Problem4();
+
+    @Test
+    public void createMapTest(){
+        String expected = "{a=4, b=2}";
+        String actual = problem4.getMapOfCharacterAndAppearance("aabbaa").toString();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void checkIfStringCanBePalindrome1Test(){
+        String testString = "aaabbbb";
+        String expected = "YES";
+
+        String actual = problem4.canStringBePalindrome(testString);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void checkIfStringCanBePalindrome2Test(){
+        String testString = "cdefghmnopqrstuvw";
+        String expected = "NO";
+
+        String actual = problem4.canStringBePalindrome(testString);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void checkIfStringCanBePalindrome3Test(){
+        String testString = "cdcdcdcdeeeef";
+        String expected = "YES";
+
+        String actual = problem4.canStringBePalindrome(testString);
+
+        Assert.assertEquals(expected,actual);
+    }
 }
